896283976fff9ada957145cf5c8342b7f764bae0
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-30 21:37:01 -0600

    update.

--- problem2.js
+++ problem2.js
@@ -1,10 +1,18 @@
 //Define a function that takes in an array of elements and joins them together into a string.
 
-let colors = ["Red", "Green", "White", "Black"];
+/*let colors = ["Red", "Green", "White", "Black"];
 
 function joinColors (arr) {
 	return arr.toString();
 }
 
 //console.log(colors.toString());
-console.log(joinColors(colors));
+console.log(joinColors(colors));*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function joinFruits(arr) {
+	return arr.join();
+}
+
+console.log(joinFruits(fruits));
