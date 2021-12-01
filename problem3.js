8432a6940f489a6c95dca6cb3dc21eaf77b9f0f8
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-12-01 00:18:58 -0600

    update.

--- problem3.js
+++ problem3.js
@@ -1,12 +1,25 @@
 //Define a function that takes in an array and returns the sorted version of that array.
 
 
-const numbers = [-3,8,7,6,5,-4,3,2,1];
+/*const numbers = [-3,8,7,6,5,-4,3,2,1];
 numbers.sort(function(a, b){
 return a - b;
 });
 
-console.log(numbers);
+console.log(numbers);*/
+
+let numbers = [-3,8,7,6,5,-4,3,2,1];
+
+const sortNumbers = numbers.sort(function(a,b){
+	if (a > b) {
+		return 1;
+	}else if (a < b) {
+		return -1;
+	}else {
+		return 0;
+	}
+});
+console.log(sortNumbers);
 
 /*numbers.sort(function(a, b){return a-b});
 
