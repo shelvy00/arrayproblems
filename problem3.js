9cbaa3e0ff2381a0736a7925864946a95433bd89
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-12-01 19:44:06 -0600

    update.

--- problem3.js
+++ problem3.js
@@ -10,7 +10,8 @@ console.log(numbers);*/
 
 let numbers = [-3,8,7,6,5,-4,3,2,1];
 
-const sortNumbers = numbers.sort(function(a,b){
+function sortNumbers (arr)
+ return arr.sort(function(a,b){
 	if (a > b) {
 		return 1;
 	}else if (a < b) {
@@ -19,7 +20,7 @@ const sortNumbers = numbers.sort(function(a,b){
 		return 0;
 	}
 });
-console.log(sortNumbers);
+console.log(sortNumbers(numbers));
 
 /*numbers.sort(function(a, b){return a-b});
 
