a18ba3a95b5d696193cea3d13895ac2cbd3ba284
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-12-07 22:18:54 -0600

    update.

--- problem3.js
+++ problem3.js
@@ -12,12 +12,12 @@ let numbers = [-3,8,7,6,5,-4,3,2,1];
 
 function sortNumbers (arr){
   return arr.sort(function(a,b){
-  if (a > b) {
-  return 1;
+   if (a > b) {
+    return 1;
   }else if (a < b) {
-  return -1;
+    return -1;
   }else {
-  return 0;
+    return 0;
   }
  });
 }  
